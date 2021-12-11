e39b5d10611da63778fe3ce202618b4a08515089
Jacob Frost <36671442+frosty656@users.noreply.github.com>
2021-12-11 16:40:44 -0500

    List is now sorted

--- App.tsx
+++ App.tsx
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { StyleSheet, Text, TextInput, View } from "react-native";
-import Slider from "@react-native-community/slider";
 import { allItems, resources } from "./ItemInfo";
 import { Picker } from "@react-native-picker/picker";
 
 export default function App() {
   interface Ingredient {
-    name: string,
-    amount: number,
-    depth: number,
+    name: string;
+    amount: number;
+    depth: number;
     numberOfBuildings: number;
+    building: string;
   }
 
   const [currentItem, setCurrentItem] = useState("Wood Plank");
@@ -21,25 +21,58 @@ export default function App() {
   const [industrialFactoryLevel, setIndustrialFactoryLevel] = useState(4);
   const [forgeLevel, setForgeLevel] = useState(4);
   const [manufacturerLevel, setManufacturerLevel] = useState(2);
+  const [extractorLevel, etExtractorLevel] = useState(4);
 
-  var ingList:Ingredient[] = [];
+  var ingList: Ingredient[] = [];
 
   // If any params are updated then we want to recalculate the list
-  useEffect(()=>{
-    ingList = []
-    GenerateList(currentItem, amount)
-    setIngredients(ingList)
-  },[workshopLevel, furnaceLevel, machineShopLevel, industrialFactoryLevel, forgeLevel, manufacturerLevel, amount, currentItem])
+  useEffect(() => {
+    ingList = [];
 
-  useEffect(()=>{
-    if(ingList.length > 0){
-      return
+    const ingInfo = allItems.find((item) => {
+      return item.name == currentItem;
+    });
+
+    const buildingLevel = getBuildingLevel(ingInfo!.building);
+    const multiplier = levelMultiplier(buildingLevel!);
+    const numberOfBuildings = amount / ingInfo!.itemsPerMin / multiplier;
+
+    var building = ingInfo!.building;
+    addIng(currentItem, amount, 0, numberOfBuildings, building);
+    GenerateList(currentItem, amount);
+    setIngredients(ingList);
+  }, [
+    workshopLevel,
+    furnaceLevel,
+    machineShopLevel,
+    industrialFactoryLevel,
+    forgeLevel,
+    manufacturerLevel,
+    amount,
+    currentItem,
+  ]);
+
+  useEffect(() => {
+    if (ingList.length > 0) {
+      return;
     }
-  }, [ingList])
+  }, [ingList]);
 
-  const addIng = (name: string, amount: number, depth: number, numberOfBuildings: number) => {
-    ingList.push({name: name, amount: amount, depth: depth, numberOfBuildings: numberOfBuildings})
-  }
+  const addIng = (
+    name: string,
+    amount: number,
+    depth: number,
+    numberOfBuildings: number,
+    building: string
+  ) => {
+    ingList.push({
+      name: name,
+      amount: amount,
+      depth: depth,
+      numberOfBuildings: Math.ceil(numberOfBuildings),
+      building: building,
+    });
+  };
 
   function getBuildingLevel(name: string) {
     switch (name.toLowerCase()) {
@@ -55,6 +88,8 @@ export default function App() {
         return forgeLevel;
       case "manufacturer":
         return manufacturerLevel;
+      case "extractor":
+        return extractorLevel;
     }
   }
 
@@ -80,9 +115,17 @@ export default function App() {
 
     itemInfo?.ingredientList.forEach((ingredient) => {
       const requireAmountPerMin = ingredient.amount * amountPerMin;
+      const buildingLevel = getBuildingLevel("extractor");
+      const multiplier = levelMultiplier(buildingLevel!);
+      const numberOfBuildings = Math.ceil(amount / (7.5 * multiplier));
       if (resources.includes(ingredient.name)) {
-
-        addIng(ingredient.name, requireAmountPerMin, depth, 0)
+        addIng(
+          ingredient.name,
+          requireAmountPerMin,
+          depth,
+          numberOfBuildings,
+          "Extractor"
+        );
       } else {
         const ingInfo = allItems.find((item) => {
           return item.name == ingredient.name;
@@ -90,10 +133,17 @@ export default function App() {
 
         const buildingLevel = getBuildingLevel(ingInfo!.building);
         const multiplier = levelMultiplier(buildingLevel!);
-        const numberOfBuildings = requireAmountPerMin / ingInfo!.itemsPerMin / multiplier;
-        addIng(ingredient.name, requireAmountPerMin, depth, numberOfBuildings)
+        const numberOfBuildings =
+          requireAmountPerMin / ingInfo!.itemsPerMin / multiplier;
+        addIng(
+          ingredient.name,
+          requireAmountPerMin,
+          depth,
+          numberOfBuildings,
+          ingInfo!.building
+        );
 
-        GenerateList(ingredient.name, requireAmountPerMin, depth + 1)
+        GenerateList(ingredient.name, requireAmountPerMin, depth + 1);
       }
     });
   }
@@ -101,58 +151,68 @@ export default function App() {
   return (
     <View style={{ alignItems: "center" }}>
       <View style={{ alignItems: "center" }}>
-      <TextInput
-        keyboardType='numeric'
-        onChangeText={(input)=>{
-          setAmount(Number(input.replace(/[^0-9]/g, '')))
-        }}
-        value={amount.toString()}
-      />
-        <View>
-        <Slider
-          style={{ width: 300 }}
-          value={amount}
-          onValueChange={(value) => {
-            setAmount(Math.round(value));
-          }}
-          maximumValue={1000}
-          minimumValue={0}
-        />
-      </View>
-      </View>
-      <Picker
-        style={{
-          marginVertical: 30,
-          width: 300,
-          padding: 10,
-          borderWidth: 1,
-          borderColor: "#666",
-          borderRadius: 5,
-        }}
-        selectedValue={currentItem}
-        onValueChange={(itemValue, itemIndex) => setCurrentItem(itemValue)}
-        itemStyle={{ borderColor: "red", borderWidth: 2, borderRadius: 5 }}
-      >
-        
-        
-        {allItems.map((data) => {
-          return <Picker.Item label={data.name} value={data.name} />;
-        })}
-      </Picker>
-      
+        <View
+          style={{ flexDirection: "row", alignItems: "center", padding: 5 }}
+        >
+          <Text style={{ paddingRight: 5 }}>Items/Min</Text>
+          <TextInput
+            style={{
+              width: 75,
+              padding: 10,
+              borderColor: "black",
+              borderRadius: 5,
+              borderWidth: 1,
+              height: 40,
+            }}
+            keyboardType="numeric"
+            onChangeText={(input) => {
+              setAmount(Number(input.replace(/[^0-9]/g, "")));
+            }}
+            value={amount.toString()}
+          />
+          <View style={{ width: 5 }} />
 
-      <Text>
-        We want {amount} {currentItem}
-      </Text>
-        <Text>------------------------</Text>
-        <View style={{alignItems:'flex-start'}}>
-      {ingredients.map((data) => {
-        if(data.amount == 0){
-          return null;
-        }
-          return <Text style={{paddingLeft: data.depth*10}}>{data.amount} {data.name}</Text>;
-        })}
+          <Picker
+            style={{
+              marginVertical: 30,
+              width: 300,
+              padding: 10,
+              borderWidth: 1,
+              borderColor: "#666",
+              borderRadius: 5,
+              height: 40,
+            }}
+            selectedValue={currentItem}
+            onValueChange={(itemValue, itemIndex) => setCurrentItem(itemValue)}
+            itemStyle={{ borderColor: "red", borderWidth: 2, borderRadius: 5 }}
+          >
+            {allItems
+              .sort((a, b) => {
+                return a.name.localeCompare(b.name);
+              })
+              .map((data) => {
+                return <Picker.Item label={data.name} value={data.name} />;
+              })}
+          </Picker>
         </View>
+      </View>
+      <Text>
+        We want {amount} {currentItem} a minute
+      </Text>
+      <Text>------------------------</Text>
+      <View style={{ alignItems: "flex-start" }}>
+        {ingredients.map((data) => {
+          if (data.amount == 0) {
+            return null;
+          }
+          return (
+            <Text style={{ paddingLeft: data.depth * 10 }}>
+              {data.amount} {data.name} ({data.numberOfBuildings}{" "}
+              {data.building})
+            </Text>
+          );
+        })}
+      </View>
     </View>
   );
 }
